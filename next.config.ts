a57ef4a95d579d2f9c2f075631c80a5bd8eb5a33
Le22 <louis-etienne.girard@epitech.eu>
2026-03-13 08:15:43 +0100

    feat : init pwa

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import { NextConfig } from "next";
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["mqtt"],
 };
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+export default withPWA(withNextIntl(nextConfig));
