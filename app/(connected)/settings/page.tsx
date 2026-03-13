acd5abbb1bfc9dec2f1491ca06660210108a278d
Le22 <louis-etienne.girard@epitech.eu>
2026-03-13 08:21:18 +0100

    feat : delete unused files

--- app/(connected)/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-const SettingsPage = () => {
-  return (
-    <div className="text-black flex items-start gap-20 p-10">
-      <h1>Paramètres</h1>
-    </div>
-  );
-};
-
-export default SettingsPage;
